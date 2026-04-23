d4fb51a9a93d67d094f81a9bbe6bef8568ee41e2
Qedi-novara <tinashesithole35@gmail.com>
2026-04-23 23:17:00 +0200

    Delete frontend directory

--- frontend/api/grid.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    harare: 500 + Math.random() * 50,
-    bulawayo: 300 + Math.random() * 30,
-    mutare: 180 + Math.random() * 20
-  });
-}
